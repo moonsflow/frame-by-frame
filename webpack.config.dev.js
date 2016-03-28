b25dad6b1c195256af0ba25d16e206f4efcaef2d
dmoon <dmoon@gcfund.org>
2016-03-28 22:05:14 +0900

    added scss loader

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,52 +1,52 @@
 var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
 
   entry: ['webpack-hot-middleware/client',
-          './client/index.js'
+          './client/index.js',
   ],
 
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/dist/'
+    publicPath: '/dist/',
   },
 
   resolve: {
-    extensions: ['', '.js', 'jsx']
+    extensions: ['', '.js', 'jsx'],
   },
 
   module: {
     loaders: [
       {
-        test: /\.scss/,
-        loader: 'style!css!sass',
+        test: /\.scss$/i,
+        loader: ExtractTextPlugin.extract('style', 'css!sass'),
       },
       {
         test: /\.jsx*$/,
         exclude: [/node_modules/, /.+\.config.js/],
         loader: 'babel',
         query: {
-          presets: ['react-hmre']
-        }
-      }
-    ]
+          presets: ['react-hmre'],
+        },
+      },
+    ],
   },
-  
   plugins: [
-      new HtmlWebpackPlugin({
-        template: './public/index.html'
-      }),
-      new webpack.HotModuleReplacementPlugin(),
-      new webpack.DefinePlugin({
-        'precess.env': {
-          CLIENT: JSON.stringify(true)
-        }
-      })
-  ]
-
+    new HtmlWebpackPlugin({
+      template: './static/index.html',
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'precess.env': {
+        CLIENT: JSON.stringify(true),
+      },
+    }),
+    new ExtractTextPlugin('styles.css'),
+  ],
 };
